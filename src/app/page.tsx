c5e64ff7ad2ad65d8b106b9ddcc5cd0a1eeefb45
Brwa Yaseen <brwayasin29@gmail.com>
2024-05-16 14:12:30 +0300

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
 
+
+const mockUrls = [
+  "https://utfs.io/f/bdd656d1-9bf8-4a5b-86cc-fcd992eea79f-fvxwhl.jpg",
+  "https://utfs.io/f/b1438875-3c04-462b-8bdb-371654981faf-gi2no9.jpg",
+  "https://utfs.io/f/e584f2cd-64f0-4a28-be4e-944ec733b951-e60hl3.jpg",
+  "https://utfs.io/f/160be6fb-205a-484b-90be-85f3c5392000-59p783.jpg"
+];
+
+const mockImages = mockUrls.map((url, index)=> ({
+  id:index + 1,
+  url,
+}))
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      What up
+    <main className="">
+     <div className=" flex flex-wrap gap-4 mt-5">
+      {[...mockImages,...mockImages,...mockImages].map((image)=>(
+        <div key={image.id} className="w-48">
+          <img src={image.url}  alt="Images"/>
+        </div>
+      ))}
+     </div>
     </main>
   );
 }
