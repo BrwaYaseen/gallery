e2a49e87ee545a9e658ac8a3d738952398549eb9
Brwa Yaseen <brwayasin29@gmail.com>
2024-05-16 15:38:47 +0300

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const mockImages = mockUrls.map((url, index)=> ({
 export default async function HomePage() {
 
   const posts = await db.query.posts.findMany()
-  console.log(posts)
+  
 
   return (
     <main className="">
