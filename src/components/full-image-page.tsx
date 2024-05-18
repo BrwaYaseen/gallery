f9db12bf33c7f324166d06c40f17d3cd3a2db782
Brwa Yaseen <brwayasin29@gmail.com>
2024-05-18 16:16:33 +0300

    new

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,19 +1,31 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
+
 
 export default async function ImagePageView(props: {id: number})
     {
    
-
-    const image = await getImage(props.id)    
+    const image = await getImage(props.id)
+    
+    
+    const uploaderInfo = await clerkClient.users.getUser(image.userId)
     return (
         <div className=" flex h-full w-full min-w-0">
             <div className="flex flex-shrink justify-center items-center">
              <img src={image.url} className=" flex-shrink object-contain" />
              </div>
 
-             <div className=" flex w-48 flex-col border-l">
-                <div className=" text-xl font-bold">
+             <div className=" flex w-48 flex-col flex-shrink-0 border-l">
+                <div className=" text-lg border-b text-center p-2">
                     {image.name}
+                </div>
+                <div className=" flex flex-col p-2 text-center border-b">
+                    <span>Uploaded By: </span>
+                    <span> {uploaderInfo.fullName}</span>
+                </div>
+                <div className=" flex flex-col p-2 text-center">
+                    <span>Published On: </span>
+                    <span> {new Date(image.createdAt).toLocaleDateString()}</span>
                 </div>
              </div>
         </div>
