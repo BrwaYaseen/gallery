1bb2c30ffdf8c9f3d15d8c9f08a7a7bd5b7d1e08
Brwa Yaseen <brwayasin29@gmail.com>
2024-05-23 14:51:51 +0300

    fixed

--- src/server/ratelimit.ts
+++ src/server/ratelimit.ts
@@ -1,12 +1,9 @@
-// rateLimiter.ts
 import type {IRateLimiterOptions} from 'rate-limiter-flexible';
 import { RateLimiterMemory } from 'rate-limiter-flexible';
 
-// Define the rate limiter configuration with proper types
 const rateLimiterConfig: IRateLimiterOptions = {
-  points: 2, // Number of requests
+  points: 10, // Number of Upload requests
   duration: 60, // Per 60 seconds
 };
 
-// Create a typed RateLimiterMemory instance
 export const rateLimiter = new RateLimiterMemory(rateLimiterConfig);
